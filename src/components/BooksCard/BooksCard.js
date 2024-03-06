c15b98280d65984998ae258528195c062ccd04ce
Svetlana Lutkova <lilissiya@yandex.ru>
2024-03-07 01:28:18 +0500

    Исправить кнопку удаления

--- src/components/BooksCard/BooksCard.js
+++ src/components/BooksCard/BooksCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './BooksCard.css';
 
 function BooksCard({ currentUser, book, onCardClick, onCardDelete }) {
-  const isOwn = book.owner.id === currentUser.id;
 
   function handleClick() {
     onCardClick(book);
@@ -14,8 +13,8 @@ function BooksCard({ currentUser, book, onCardClick, onCardDelete }) {
 
   return (
     <div className="element">
+      <button type="button" className="element__button element__button_delete element__button_delete_active" aria-label="Удалить карточку места" onClick={handleDeleteClick} />
       <img className="element__image" src={book.image} alt={book.name} onClick={handleClick} />
-      {isOwn && <button type="button" className="element__button element__button_delete element__button_delete_active" aria-label="Удалить карточку места" onClick={handleDeleteClick} />}
       <div className="element__description">
         <h2 className="title element__title">{book.name}</h2>
         <h3 className="subtitle element__subtitle">{book.author}</h3>
